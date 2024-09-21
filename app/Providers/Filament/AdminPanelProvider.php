677910cfd995052afa3600c68df007b48d7a2574
bymrbanks <bymrbanks@gmail.com>
2024-09-21 10:48:29 -0500

    updated logo

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -26,6 +26,7 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('admin')
             ->path('admin')
+            ->brandName('The Ackee Walk')
             ->login()
             ->colors([
                 'primary' => Color::Amber,
